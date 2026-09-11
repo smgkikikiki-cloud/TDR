51ceaf584b25013dbaeddee71163d17b4a90f4c8
smgkikikiki-cloud <smgkikikiki@gmail.com>
2026-09-11 15:35:43 +0700

    Load SaaS public chrome styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import "./catalog-v12.css";
 import "./storefront.css";
 import "./compare.css";
+import "./saas-shell.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { PublicChrome } from "@/components/PublicChrome";
